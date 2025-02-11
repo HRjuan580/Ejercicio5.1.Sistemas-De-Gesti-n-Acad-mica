df9d352b5891fd16fb95b440dd5fffb46574dce7
HRJuan580 <jherrel580@g.educaand.es>
2025-02-11 16:16:09 +0100

    falta la class Persona.kt

---
+++ src/Estudiante.kt
@@ -0,0 +1,9 @@
+class Estudiante(nombre: String, edad: Int, id: String, val curso: String, val calificacionPromedio: Double) : Persona(nombre, edad, id) {
+    override fun mostrarRol() {
+        println("Rol: Estudiante")
+    }
+
+    fun mostrarCalificacion() {
+        println("Calificación promedio: $calificacionPromedio")
+    }
+}
